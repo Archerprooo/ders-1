010369988077d62f3997df01967b4ee5f9027f07
Archerprooo <asdostum4@gmail.com>
2024-03-23 10:18:22 +0300

    a

--- import datetime.py
+++ import datetime.py
@@ -1,10 +1,14 @@
 import datetime
 import random
+import time
 a = datetime.datetime.now().strftime("%d")
 b = datetime.datetime.now().strftime("%m")
 c = datetime.datetime.now().strftime("%y")
-a = random.randint(1,31)
-b = random.randint(1,12)
-c = random.randint(2024,2029)
-print ("randevunuz = ",a,b,c)
+d = int(input("doğduğun yılın son iki hanesi ne " ))
+e = int(input("doğduğun ay ne "))
+f = int(input("doğduğun gün ne "))
+g = int(a) - f
+l = int(b) - e
+y = int(c) - d
+print ("hayatta olduğun zaman",g,l,y)
     
