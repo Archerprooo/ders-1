75ebfa418cabc5cbd34cdde43dbbb3eca8dcf5a6
Archerprooo <asdostum4@gmail.com>
2024-03-23 10:29:08 +0300

    p

--- import datetime.py
+++ import datetime.py
@@ -4,7 +4,7 @@ import time
 a = datetime.datetime.now().strftime("%d")
 b = datetime.datetime.now().strftime("%m")
 c = datetime.datetime.now().strftime("%Y")
-d = int(input("doğduğun yı ne " ))
+d = int(input("doğduğun yıl ne " ))
 e = int(input("doğduğun ay ne "))
 f = int(input("doğduğun gün ne "))
 g = int(a) - f
