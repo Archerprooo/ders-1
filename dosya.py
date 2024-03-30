6659f209b0326ec260f11cc15302c396b564dff4
Archerprooo <asdostum4@gmail.com>
2024-03-30 10:50:12 +0300

    u

---
+++ dosya.py
@@ -0,0 +1,10 @@
+abc = open("rehber.txt","w")
+for a in range (100):
+    abc.write(f"{a}")
+abc.close()
+try:
+    abc =open("rehber.txt","r")
+    print(abc.read())
+    abc.close()
+except:
+    print("hata")
