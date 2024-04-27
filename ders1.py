1311de8e604a140619cb22ea7f988800e64f3cfc
Archerprooo <Asdostum4@gmail.com>
2024-04-27 11:54:45 +0300

    A

--- ders1.py
+++ ders1.py
@@ -1,13 +1,26 @@
 from PyQt6.QtWidgets import *
 import sys
+
 aa = QApplication([])
 ww =QWidget()
+
 icerik = QVBoxLayout()
+icerik2 = QHBoxLayout()
+
 icerik.addWidget(QPushButton('tıkla'))
 icerik.addWidget(QPushButton('dene'))
 icerik.addWidget(QLabel('Bilgi'))
+
+icerik2.addWidget(QPushButton('tıkla'))
+icerik2.addWidget(QPushButton('dene'))
+icerik2.addWidget(QLabel('Bilgi'))
+
+
 ww.setLayout(icerik)
+ww.setLayout(icerik2)
+
 ww.show()
+
 aa.exec()
 
 
