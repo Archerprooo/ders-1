3faef993a397a4f00e13523a50f7e8b122a46f3e
Archerprooo <Asdostum4@gmail.com>
2024-04-27 11:57:08 +0300

    z

--- ders1.py
+++ ders1.py
@@ -3,6 +3,7 @@ import sys
 
 aa = QApplication([])
 ww =QWidget()
+ww1 =QWidget()
 
 icerik = QVBoxLayout()
 icerik2 = QHBoxLayout()
@@ -10,16 +11,17 @@ icerik2 = QHBoxLayout()
 icerik.addWidget(QPushButton('tıkla'))
 icerik.addWidget(QPushButton('dene'))
 icerik.addWidget(QLabel('Bilgi'))
+ww.setLayout(icerik)
 
 icerik2.addWidget(QPushButton('tıkla'))
 icerik2.addWidget(QPushButton('dene'))
 icerik2.addWidget(QLabel('Bilgi'))
 
 
-ww.setLayout(icerik)
-ww.setLayout(icerik2)
+ww1.setLayout(icerik2)
 
 ww.show()
+ww1.show()
 
 aa.exec()
 
