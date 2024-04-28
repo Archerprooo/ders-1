f6d6f1c29ce952ae90373212a049e2e0b0a2f37a
Archerprooo <Asdostum4@gmail.com>
2024-04-28 09:58:38 +0300

    düzeltme

--- ders1.py
+++ ders1.py
@@ -1,31 +1,36 @@
 from PyQt6.QtWidgets import *
 import sys
-def özelleştir():
-    dosya = open("profil_Bilgileri","w")
-    dosya.write("")
-    dosya.close()
+Anauygulama = QApplication([])
+Anapencere = QWidget()
+Anaekran = QVBoxLayout()
+class özelleştir(QMainWindow):
+    def __init__(self,xx="Başlıksız"):
+        dosya = open("profil_Bilgileri","w")
+        dosya.write("")
+        dosya.close()
 
-    uygulama = QApplication([])
-    pencere =QWidget()
+        uygulama = QApplication([])
+        pencere =QWidget()
 
 
-    kalıp = QVBoxLayout()
-    pencere.setWindowTitle("Profil")
+        kalıp = QVBoxLayout()
+        pencere.setWindowTitle("Profil")
 
-    kalıp.addWidget(QLineEdit('Kullanızı adınız...'))
-    kalıp.addWidget(QLineEdit('Şifreniz...'))
-    kalıp.addWidget(QLineEdit('Mailiniz...'))
+        kalıp.addWidget(QLineEdit('Kullanıcı adınız...'))
+        kalıp.addWidget(QLineEdit('Şifreniz...'))
+        kalıp.addWidget(QLineEdit('Mailiniz...'))
 
-    kalıp.addWidget(QLabel('Doğum Tarihiniz'))
-    kalıp.addWidget(QScrollBar())
+        kalıp.addWidget(QLabel('Doğum Tarihiniz'))
+        kalıp.addWidget(QScrollBar())
 
-    kalıp.addWidget(QLabel('Arka plan renginiz'))
-    kalıp.addWidget(QColorDialog())
+        kalıp.addWidget(QLabel('Arka plan renginiz'))
+        kalıp.addWidget(QColorDialog())
 
-    pencere.setLayout(kalıp)
+        pencere.setLayout(kalıp)
 
-    pencere.show()
+        pencere.show()
 
-    uygulama.exec()
+        uygulama.exec()
+
 özelleştir()
 
